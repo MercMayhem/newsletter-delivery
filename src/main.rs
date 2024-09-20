bb70eedcfcc4672f77d45468f71c53f82e6ecce3
MercMayhem <amanrao032@gmail.com>
2024-09-20 21:22:58 +0530

    updated main to launch worker task along with server

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::fmt::{Debug, Display};
+
+use newsletter::issue_delivery_worker::run_worker_until_stopped;
 use newsletter::startup::Application;
 use newsletter::configuration::get_configuration;
 use newsletter::telemetry::{get_subscriber, init_subscriber};
+use tokio::task::JoinError;
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()>{
@@ -8,8 +12,45 @@ async fn main() -> anyhow::Result<()>{
     init_subscriber(subscriber);
 
     let config = get_configuration().expect("Failed to get configuration");
-    let application = Application::build(config).await?;
+    let application = Application::build(config.clone())
+            .await?;
 
-    application.run_until_stopped().await?;
+    let application_task = tokio::spawn(application.run_until_stopped());
+    let worker_task = tokio::spawn(run_worker_until_stopped(config));
+
+    tokio::select! {
+        o = application_task => report_exit("API", o),
+        o = worker_task => report_exit("Background Worker", o),
+    }
+
     Ok(())
+}
+
+fn report_exit(
+    task_name: &str,
+    outcome: Result<Result<(), impl Debug + Display>, JoinError>
+){
+    match outcome {
+        Ok(Ok(())) => {
+            tracing::info!("{} has exited", task_name)
+        },
+
+        Ok(Err(e)) => {
+            tracing::error!(
+                error.cause_chain = ?e,
+                error.message = %e,
+                "{} failed",
+                task_name
+            )
+        },
+
+        Err(e) => {
+            tracing::error!(
+            error.cause_chain = ?e,
+            error.message = %e,
+            "{}' task failed to complete",
+            task_name
+            )
+        }
+    }
 }
